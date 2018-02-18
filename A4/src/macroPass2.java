0f476ccc579a845ca45a11a9b32b23b8418c7908
Harshit Sinha <sinhov@Harshits-MacBook-Pro.local>
2018-02-18 23:40:44 +0530

    bug fixes

--- A4/src/macroPass2.java
+++ A4/src/macroPass2.java
@@ -1,11 +1,9 @@
 //Sinhov : "The girl told me, take off your jacket. I said, Babes, man's not hot, never hot."
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
+import java.util.Vector;
 
-public class macroPass2 {
+public class Main {
 	public static void main(String[] Args) throws IOException{
 		BufferedReader b1 = new BufferedReader(new FileReader("intermediate.txt"));
 		BufferedReader b2 = new BufferedReader(new FileReader("mnt.txt"));
@@ -14,53 +12,71 @@ public class macroPass2 {
 		FileWriter f1 = new FileWriter("Pass2.txt");
 		HashMap<Integer,String> aptab=new HashMap<Integer,String>();
 		HashMap<String,Integer> aptabInverse=new HashMap<String,Integer>();
-		HashMap<String,Integer> mnt=new HashMap<String,Integer>();
-		String s,s1,s2;
-		int i,pp,kp,kpdtp,mdtp,ppa,kpa;
+		HashMap<String,Integer> mdtpHash=new HashMap<String,Integer>();
+		HashMap<String,Integer> kpdtpHash=new HashMap<String,Integer>();
+		HashMap<String,Integer> kpHash=new HashMap<String,Integer>();
+		HashMap<String,Integer> macroNameHash=new HashMap<String,Integer>();
+		Vector<String>mdt=new Vector<String>();
+		Vector<String>kpdt=new Vector<String>();
+		String s,s1;
+		int i,pp,kp,kpdtp,mdtp,paramNo;
+		while((s=b3.readLine())!=null)
+			mdt.addElement(s);
+		while((s=b4.readLine())!=null)
+			kpdt.addElement(s);
+		while((s=b2.readLine())!=null){
+			String word[]=s.split("\t");
+			s1=word[0]+word[1];
+			macroNameHash.put(word[0],1);
+			kpHash.put(s1,Integer.parseInt(word[2]));
+			mdtpHash.put(s1,Integer.parseInt(word[3]));
+			kpdtpHash.put(s1,Integer.parseInt(word[4]));
+		}
 		while((s=b1.readLine())!=null){
-			s1=b2.readLine();
-			String word[]=s1.split("\t");
-			pp=Integer.parseInt(word[1]);
-			kp=Integer.parseInt(word[2]);
-			if(s.split("\\s").length==1){
-				while((s2=b3.readLine())!=null&&s2.compareToIgnoreCase("MEND")!=0){
+			String b1Split[]=s.split("\\s");
+			if(macroNameHash.containsKey(b1Split[0])){
+				pp= b1Split[1].split(",").length-b1Split[1].split("=").length+1;
+				kp=kpHash.get(b1Split[0]+Integer.toString(pp));
+				mdtp=mdtpHash.get(b1Split[0]+Integer.toString(pp));
+				kpdtp=kpdtpHash.get(b1Split[0]+Integer.toString(pp));
+				String actualParams[]=b1Split[1].split(",");
+				paramNo=1;
+				for(int j=0;j<pp;j++){
+					aptab.put(paramNo, actualParams[paramNo-1]);
+					aptabInverse.put(actualParams[paramNo-1],paramNo);
+					paramNo++;
+				}
+				i=kpdtp-1;
+				for(int j=0;j<kp;j++){
+					String temp[]=kpdt.get(i).split("\t");
+					aptab.put(paramNo,temp[1]);
+					aptabInverse.put(temp[0],paramNo);
+					i++;
+					paramNo++;
+				}
+				i=pp+1;
+				while(i<=actualParams.length){
+					String initializedParams[]=actualParams[i-1].split("=");
+					aptab.put(aptabInverse.get(initializedParams[0].substring(1,initializedParams[0].length())),initializedParams[1].substring(0,initializedParams[1].length()));
+					i++;
+				}
+				i=mdtp-1;
+				while(mdt.get(i).compareToIgnoreCase("MEND")!=0){
 					f1.write("+ ");
-					for(i=0;i<s2.length();i++)
-						f1.write(s2.charAt(i));
+					for(int j=0;j<mdt.get(i).length();j++){
+						if(mdt.get(i).charAt(j)=='#')
+							f1.write(aptab.get(Integer.parseInt("" + mdt.get(i).charAt(++j))));
+						else
+							f1.write(mdt.get(i).charAt(j));
+					}
+					f1.write("\n");
+					i++;
 				}
-				continue;
+				aptab.clear();
+				aptabInverse.clear();
 			}
-			String actualParams[]=s.split("\\s")[1].split(",");
-			i=1;
-			for(int j=0;j<pp;j++){
-				aptab.put(i, actualParams[i-1]);
-				aptabInverse.put(actualParams[i-1],i);
-				i++;
-			}
-			for(int j=0;j<kp&&(s2=b4.readLine())!=null;j++){
-				String kpdt[]=s2.split("\t");
-				aptab.put(i,kpdt[1]);
-				aptabInverse.put(kpdt[0],i);
-				i++;
-			}
-			i=pp+1;
-			while(i<=actualParams.length){
-				String initializedParams[]=actualParams[i-1].split("=");
-				aptab.put(aptabInverse.get(initializedParams[0].substring(1,initializedParams[0].length())),initializedParams[1].substring(1,initializedParams[1].length()));
-				i++;
-			}
-			while((s2=b3.readLine())!=null&&s2.compareToIgnoreCase("MEND")!=0){
-				f1.write("+ ");
-				for(i=0;i<s2.length();i++){
-					if(s2.charAt(i)=='#')
-						f1.write(aptab.get(Integer.parseInt(String.valueOf(s2.charAt(++i)))));
-					else
-						f1.write(s2.charAt(i));
-				}
-				f1.write("\n");
-			}
-			aptab.clear();
-			aptabInverse.clear();
+			else
+				f1.write("+ "+s+"\n");
 		}
 		b1.close();
 		b2.close();
